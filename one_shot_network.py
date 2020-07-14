9e7597fc420e07ee7e8fd6f5202de68ae2344a97
vltanh <vltheanh030698@gmail.com>
2020-07-14 16:10:17 +0700

    add visualization

--- one_shot_network.py
+++ one_shot_network.py
@@ -2,9 +2,9 @@ import torch.nn as nn
 import torch
 import numpy as np
 import torch.nn.functional as F
+import math
 
-
-#code of dilated convolution part is referenced from https://github.com/speedinghzl/Pytorch-Deeplab
+# code of dilated convolution part is referenced from https://github.com/speedinghzl/Pytorch-Deeplab
 
 affine_par = True
 
@@ -16,10 +16,21 @@ def outS(i):
     i = (i+1)/2
     return i
 
+
 def conv3x3(in_planes, out_planes, stride=1):
     "3x3 convolution with padding"
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
                      padding=1, bias=False)
+
+
+def convert_image_np(inp):
+    """Convert a Tensor to numpy image."""
+    inp = inp.numpy().transpose((1, 2, 0))
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    inp = std * inp + mean
+    inp = np.clip(inp, 0, 1)
+    return inp
 
 
 class BasicBlock(nn.Module):
@@ -28,10 +39,10 @@ class BasicBlock(nn.Module):
     def __init__(self, inplanes, planes, stride=1, downsample=None):
         super(BasicBlock, self).__init__()
         self.conv1 = conv3x3(inplanes, planes, stride)
-        self.bn1 = nn.BatchNorm2d(planes, affine = affine_par)
+        self.bn1 = nn.BatchNorm2d(planes, affine=affine_par)
         self.relu = nn.ReLU(inplace=True)
         self.conv2 = conv3x3(planes, planes)
-        self.bn2 = nn.BatchNorm2d(planes, affine = affine_par)
+        self.bn2 = nn.BatchNorm2d(planes, affine=affine_par)
         self.downsample = downsample
         self.stride = stride
 
@@ -59,25 +70,25 @@ class Bottleneck(nn.Module):
 
     def __init__(self, inplanes, planes, stride=1, dilation=1, downsample=None):
         super(Bottleneck, self).__init__()
-        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, stride=stride, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes,affine = affine_par)
+        self.conv1 = nn.Conv2d(
+            inplanes, planes, kernel_size=1, stride=stride, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes, affine=affine_par)
         for i in self.bn1.parameters():
             i.requires_grad = False
 
         padding = dilation
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1,
-                               padding=padding, bias=False, dilation = dilation)
-        self.bn2 = nn.BatchNorm2d(planes,affine = affine_par)
+                               padding=padding, bias=False, dilation=dilation)
+        self.bn2 = nn.BatchNorm2d(planes, affine=affine_par)
         for i in self.bn2.parameters():
             i.requires_grad = False
         self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(planes * 4, affine = affine_par)
+        self.bn3 = nn.BatchNorm2d(planes * 4, affine=affine_par)
         for i in self.bn3.parameters():
             i.requires_grad = False
         self.relu = nn.ReLU(inplace=True)
         self.downsample = downsample
         self.stride = stride
-
 
     def forward(self, x):
         residual = x
@@ -102,37 +113,61 @@ class Bottleneck(nn.Module):
         return out
 
 
+class Memory(nn.Module):
+    def __init__(self):
+        super(Memory, self).__init__()
 
+    def forward(self, m_k, m_v, q_k):
+        # m_k: B, Dk, Hm, Wm
+        # m_v: B, Dv, Hm, Wm
+        # q_k: B, Dk, Hq, Wq
 
+        B, Dk, Hm, Wm = m_k.size()
+        _,  _, Hq, Wq = q_k.size()
+        _, Dv,  _,  _ = m_v.size()
 
+        mk = m_k.reshape(B, Dk, Hm*Wm)  # mk: B, Dk, Hm*Wm
+        mk = torch.transpose(mk, 1, 2)  # mk: B, Hm*Wm, Dk
 
+        qk = q_k.reshape(B, Dk, Hq*Wq)  # qk: B, Dk, Hq*Wq
+
+        p = torch.bmm(mk, qk)  # p: B, Hm*Wm, Hq*Wq
+        p = p / math.sqrt(Dk)  # p: B, Hm*Wm, Hq*Wq
+        p = F.softmax(p, dim=1)  # p: B, Hm*Wm, Hq*Wq
+
+        mv = m_v.reshape(B, Dv, Hm*Wm)  # mv: B, Dv, Hm*Wm
+        mem = torch.bmm(mv, p)  # B, Dv, Hq*Wq
+        mem = mem.reshape(B, Dv, Hq, Wq)  # B, Dv, Hq, Wq
+
+        return mem, p
 
 
 class ResNet(nn.Module):
     def __init__(self, block, layers, num_classes):
 
-
-
         self.inplanes = 64
         super(ResNet, self).__init__()
         self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
                                bias=False)
-        self.bn1 = nn.BatchNorm2d(64, affine = affine_par)
+        self.bn1 = nn.BatchNorm2d(64, affine=affine_par)
         for i in self.bn1.parameters():
             i.requires_grad = False
         self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1, ceil_mode=True)
+        self.maxpool = nn.MaxPool2d(
+            kernel_size=3, stride=2, padding=1, ceil_mode=True)
         self.layer1 = self._make_layer(block, 64, layers[0])
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=1, dilation=2)
+        self.layer3 = self._make_layer(
+            block, 256, layers[2], stride=1, dilation=2)
         #self.layer4 = self._make_layer(block, 512, layers[3], stride=1, dilation=4)
 
-
-        self.layer5 = nn.Sequential(nn.Conv2d(in_channels=1536, out_channels=256, kernel_size=3, stride=1, padding=2, dilation=2, bias = True),
+        self.layer5 = nn.Sequential(nn.Conv2d(in_channels=1536, out_channels=256, kernel_size=3, stride=1, padding=2, dilation=2, bias=True),
                                     nn.ReLU(),
                                     nn.Dropout2d(p=0.5))
 
-
+        # self.layer5_K = nn.Sequential(nn.Conv2d(in_channels=1536, out_channels=256, kernel_size=3, stride=1, padding=2, dilation=2, bias = True),
+        #                             nn.ReLU(),
+        #                             nn.Dropout2d(p=0.5))
 
         self.layer55 = nn.Sequential(
             nn.Conv2d(in_channels=256 * 2, out_channels=256, kernel_size=3, stride=1, padding=2, dilation=2,
@@ -140,59 +175,56 @@ class ResNet(nn.Module):
             nn.ReLU(),
             nn.Dropout2d(p=0.5))
 
-
-
-
-
         self.layer6_0 = nn.Sequential(
-            nn.Conv2d(256 , 256, kernel_size=1, stride=1, padding=0, bias=True),
+            nn.Conv2d(256, 256, kernel_size=1, stride=1, padding=0, bias=True),
             nn.ReLU(),
             nn.Dropout2d(p=0.5),
         )
 
-
         self.layer6_1 = nn.Sequential(
-            nn.Conv2d(256 , 256, kernel_size=1, stride=1, padding=0, bias=True),
+            nn.Conv2d(256, 256, kernel_size=1, stride=1, padding=0, bias=True),
             nn.ReLU(),
             nn.Dropout2d(p=0.5),
-            )
+        )
 
-        self.layer6_2 = nn.Sequential(nn.Conv2d(256 , 256 , kernel_size=3, stride=1, padding=6,dilation=6, bias=True),
-                                    nn.ReLU(),
-                                    nn.Dropout2d(p=0.5)
-                                    )
+        self.layer6_2 = nn.Sequential(nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=6, dilation=6, bias=True),
+                                      nn.ReLU(),
+                                      nn.Dropout2d(p=0.5)
+                                      )
 
         self.layer6_3 = nn.Sequential(
-            nn.Conv2d(256 , 256, kernel_size=3, stride=1, padding=12, dilation=12, bias=True),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1,
+                      padding=12, dilation=12, bias=True),
             nn.ReLU(),
             nn.Dropout2d(p=0.5)
-            )
+        )
 
         self.layer6_4 = nn.Sequential(
-            nn.Conv2d(256 , 256 , kernel_size=3, stride=1, padding=18, dilation=18, bias=True),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1,
+                      padding=18, dilation=18, bias=True),
             nn.ReLU(),
             nn.Dropout2d(p=0.5)
-            )
+        )
 
-        self.layer7=nn.Sequential(
-            nn.Conv2d(1280, 256 , kernel_size=1, stride=1, padding=0, bias=True),
+        self.layer7 = nn.Sequential(
+            nn.Conv2d(1280, 256, kernel_size=1,
+                      stride=1, padding=0, bias=True),
             nn.ReLU(),
             nn.Dropout2d(p=0.5),
 
-            )
+        )
 
-
-
-        self.residule1=nn.Sequential(
+        self.residule1 = nn.Sequential(
             nn.ReLU(),
-            nn.Conv2d(256+2,256,kernel_size=3,stride=1,padding=1,bias=True),
+            nn.Conv2d(256+2, 256, kernel_size=3,
+                      stride=1, padding=1, bias=True),
             nn.ReLU(),
             nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, bias=True)
         )
 
         self.residule2 = nn.Sequential(
             nn.ReLU(),
-            nn.Conv2d(256 , 256, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, bias=True),
             nn.ReLU(),
             nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, bias=True)
         )
@@ -204,14 +236,10 @@ class ResNet(nn.Module):
             nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, bias=True)
         )
 
+        self.layer9 = nn.Conv2d(
+            256, num_classes, kernel_size=1, stride=1, bias=True)
 
-
-
-
-        self.layer9=nn.Conv2d(256,num_classes,kernel_size=1,stride=1,bias=True)
-
-
-
+        self.memory = Memory()
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -221,27 +249,33 @@ class ResNet(nn.Module):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
 
-
     def _make_layer(self, block, planes, blocks, stride=1, dilation=1):
         downsample = None
-        if stride != 1 or self.inplanes != planes * block.expansion or dilation == 2 or dilation == 4:#注意，resnet中每个大block内的第一个bottleneck之后又个downsample
+        # 注意，resnet中每个大block内的第一个bottleneck之后又个downsample
+        if stride != 1 or self.inplanes != planes * block.expansion or dilation == 2 or dilation == 4:
             downsample = nn.Sequential(
                 nn.Conv2d(self.inplanes, planes * block.expansion,
                           kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(planes * block.expansion,affine = affine_par))
+                nn.BatchNorm2d(planes * block.expansion, affine=affine_par))
         for i in downsample._modules['1'].parameters():
             i.requires_grad = False
         layers = []
-        layers.append(block(self.inplanes, planes, stride,dilation=dilation, downsample=downsample))
+        layers.append(block(self.inplanes, planes, stride,
+                            dilation=dilation, downsample=downsample))
         self.inplanes = planes * block.expansion
         for i in range(1, blocks):
             layers.append(block(self.inplanes, planes, dilation=dilation))
 
         return nn.Sequential(*layers)
-    def _make_pred_layer(self,block, dilation_series, padding_series,num_classes):
-        return block(dilation_series,padding_series,num_classes)
 
-    def forward(self, query_rgb,support_rgb,support_mask,history_mask):
+    def _make_pred_layer(self, block, dilation_series, padding_series, num_classes):
+        return block(dilation_series, padding_series, num_classes)
+
+    def forward(self, query_rgb, support_rgb, support_mask, history_mask):
+        ref_img = support_rgb.clone()
+        ref_mask = support_mask.clone()
+        query_img = query_rgb.clone()
+
         # important: do not optimize the RESNET backbone
         query_rgb = self.conv1(query_rgb)
         query_rgb = self.bn1(query_rgb)
@@ -249,19 +283,17 @@ class ResNet(nn.Module):
         query_rgb = self.maxpool(query_rgb)
         query_rgb = self.layer1(query_rgb)
         query_rgb = self.layer2(query_rgb)
-        query_feat_layer2=query_rgb
+        query_feat_layer2 = query_rgb
         query_rgb = self.layer3(query_rgb)
         # query_rgb = self.layer4(query_rgb)
-        query_rgb=torch.cat([query_feat_layer2,query_rgb],dim=1)
+        query_rgb_ = torch.cat([query_feat_layer2, query_rgb], dim=1)
 
-        query_rgb = self.layer5(query_rgb)
+        query_rgb = self.layer5(query_rgb_)
+        # query_rgb_K = self.layer5_K(query_rgb_)
 
         feature_size = query_rgb.shape[-2:]
 
-
-
-
-        #side branch,get latent embedding z
+        # side branch,get latent embedding z
         support_rgb = self.conv1(support_rgb)
         support_rgb = self.bn1(support_rgb)
         support_rgb = self.relu(support_rgb)
@@ -272,49 +304,80 @@ class ResNet(nn.Module):
         support_rgb = self.layer3(support_rgb)
 
         #support_rgb = self.layer4(support_rgb)
-        support_rgb = torch.cat([support_feat_layer2, support_rgb], dim=1)
-        support_rgb = self.layer5(support_rgb)
+        support_rgb_ = torch.cat([support_feat_layer2, support_rgb], dim=1)
+        support_rgb = self.layer5(support_rgb_)
+        # support_rgb_K = self.layer5_K(support_rgb_)
+        support_mask = F.interpolate(
+            support_mask, support_rgb.shape[-2:], mode='bilinear', align_corners=True)
 
+        # ======== START ==============
+        if False:
+            h, w = support_rgb.shape[-2:]
+            area = F.avg_pool2d(
+                support_mask, support_rgb.shape[-2:]) * h * w + 0.0005
+            z = support_mask * support_rgb
+            z = F.avg_pool2d(input=z,
+                             kernel_size=support_rgb.shape[-2:]) * h * w / area
+            # tile for cat
+            z = z.expand(-1, -1, feature_size[0], feature_size[1])
+        else:
+            # z_K = support_mask * support_rgb_K
+            # z_V = support_mask * support_rgb
+            # z, p = self.memory(z_K, z_V, query_rgb_K)
 
-        support_mask = F.interpolate(support_mask, support_rgb.shape[-2:], mode='bilinear',align_corners=True)
-        h,w=support_rgb.shape[-2:][0],support_rgb.shape[-2:][1]
+            z = support_mask * support_rgb
+            z, viz = self.memory(z, z, query_rgb)
 
+            import matplotlib.pyplot as plt
+            for i in range(viz.size(2)):
+                plt.subplot(1, 2, 1)
+                m = torch.zeros(query_rgb.shape[-2], query_rgb.shape[-1])
+                m[i // query_rgb.shape[-1], i % query_rgb.shape[-1]] = 1
+                m = F.interpolate(m.unsqueeze(0).unsqueeze(
+                    0), (query_img.shape[-2], query_img.shape[-1])).squeeze(0).squeeze(0)
+                # f = query_img[0].permute(1, 2, 0).detach().cpu()
+                plt.imshow(convert_image_np(query_img[0].cpu()))
+                plt.imshow(m, alpha=0.5)
+                plt.subplot(1, 2, 2)
+                v = viz[0, :, i].reshape(
+                    support_rgb.shape[-2], support_rgb.shape[-1]).detach().cpu()
+                v = F.interpolate(v.unsqueeze(
+                    0).unsqueeze(0), (ref_img.shape[-2], ref_img.shape[-1])).squeeze(0).squeeze(0)
+                f = ref_img[0].permute(1, 2, 0).detach().cpu()
+                # plt.imshow(f)
+                plt.imshow(v, alpha=0.5)
+                plt.tight_layout()
+                plt.savefig(f'viz/{i:04d}')
+                # plt.show()
+                plt.close()
 
-        area = F.avg_pool2d(support_mask, support_rgb.shape[-2:]) * h * w + 0.0005
-        z = support_mask * support_rgb
-        z = F.avg_pool2d(input=z,
-                         kernel_size=support_rgb.shape[-2:]) * h * w / area
-        z = z.expand(-1, -1, feature_size[0], feature_size[1])  # tile for cat
+        # ======= END ================
 
+        out = torch.cat([query_rgb, z], dim=1)
 
-        history_mask=F.interpolate(history_mask,feature_size,mode='bilinear',align_corners=True)
-
-        out=torch.cat([query_rgb,z],dim=1)
+        history_mask = F.interpolate(
+            history_mask, feature_size, mode='bilinear', align_corners=True)
         out = self.layer55(out)
-        out_plus_history=torch.cat([out,history_mask],dim=1)
+        out_plus_history = torch.cat([out, history_mask], dim=1)
         out = out + self.residule1(out_plus_history)
         out = out + self.residule2(out)
         out = out + self.residule3(out)
 
+        global_feature = F.avg_pool2d(out, kernel_size=feature_size)
+        global_feature = self.layer6_0(global_feature)
+        global_feature = global_feature.expand(-1, -1,
+                                               feature_size[0], feature_size[1])
+        out = torch.cat([global_feature, self.layer6_1(out), self.layer6_2(
+            out), self.layer6_3(out), self.layer6_4(out)], dim=1)
+        out = self.layer7(out)
+        out = self.layer9(out)
 
-
-
-        global_feature=F.avg_pool2d(out,kernel_size=feature_size)
-        global_feature=self.layer6_0(global_feature)
-        global_feature=global_feature.expand(-1,-1,feature_size[0],feature_size[1])
-        out=torch.cat([global_feature,self.layer6_1(out),self.layer6_2(out),self.layer6_3(out),self.layer6_4(out)],dim=1)
-        out=self.layer7(out)
-
-        out=self.layer9(out)
         return out
 
 
-
-
 def Res_Deeplab(num_classes=2):
-    model = ResNet(Bottleneck,[3, 4, 6, 3], num_classes)
+    model = ResNet(Bottleneck, [3, 4, 6, 3], num_classes)
     return model
-
 
 
 if __name__ == '__main__':
@@ -338,22 +401,13 @@ if __name__ == '__main__':
         model.train()
         return model
 
-
-
-
     model = Res_Deeplab(num_classes=2).cuda()
-    model=load_resnet50_param(model)
+    model = load_resnet50_param(model)
 
-    query_rgb = torch.FloatTensor(1,3,321,321).cuda()
-    support_rgb = torch.FloatTensor(1,3,321,321).cuda()
-    support_mask = torch.FloatTensor(1,1,321,321).cuda()
+    query_rgb = torch.FloatTensor(1, 3, 321, 321).cuda()
+    support_rgb = torch.FloatTensor(1, 3, 321, 321).cuda()
+    support_mask = torch.FloatTensor(1, 1, 321, 321).cuda()
 
-    history_mask=(torch.zeros(1,2,50,50)).cuda()
+    history_mask = (torch.zeros(1, 2, 50, 50)).cuda()
 
-    pred = (model(query_rgb,support_rgb,support_mask,history_mask))
-
-
-
-
-
-
+    pred = (model(query_rgb, support_rgb, support_mask, history_mask))
--- val.py
+++ val.py
@@ -17,7 +17,6 @@ import torch.nn as nn
 import numpy as np
 
 
-
 parser = argparse.ArgumentParser()
 
 
@@ -40,8 +39,7 @@ parser.add_argument('-bs',
 parser.add_argument('-bs_val',
                     type=int,
                     help='batchsize for val',
-                    default=64)
-
+                    default=1)
 
 
 parser.add_argument('-fold',
@@ -50,34 +48,34 @@ parser.add_argument('-fold',
                     default=0)
 
 
-
 parser.add_argument('-gpu',
                     type=str,
                     help='gpu id to use',
                     default='0,1')
 
 
-
 parser.add_argument('-iter_time',
                     type=int,
-                    default=5)
+                    default=1)
 
 parser.add_argument('-w',
+                    type=str)
+
+parser.add_argument('-d',
                     type=str)
 
 
 options = parser.parse_args()
 
 
-data_dir = '../davis_challenge_2020/data/PASCAL-5i/'
+data_dir = options.d
 
 
-
-
-#set gpus
+# set gpus
 gpu_list = [int(x) for x in options.gpu.split(',')]
 os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
 os.environ['CUDA_VISIBLE_DEVICES'] = options.gpu
+
 
 def set_seed(seed):
     np.random.seed(seed)
@@ -89,8 +87,19 @@ def set_determinism():
     torch.backends.cudnn.benchmark = False
     torch.backends.cudnn.deterministic = True
 
+
+def convert_image_np(inp):
+    """Convert a Tensor to numpy image."""
+    inp = inp.numpy().transpose((1, 2, 0))
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    inp = std * inp + mean
+    inp = np.clip(inp, 0, 1)
+    return inp
+
+
 set_seed(3698)
-#set_determinism()
+# set_determinism()
 
 IMG_MEAN = [0.485, 0.456, 0.406]
 IMG_STD = [0.229, 0.224, 0.225]
@@ -108,21 +117,17 @@ cudnn.enabled = True
 
 # Create network.
 model = Res_Deeplab(num_classes=num_class)
-#load resnet-50 preatrained parameter
+# load resnet-50 preatrained parameter
 model = load_resnet50_param(model, stop_layer='layer4')
-model=nn.DataParallel(model,[0])
+model = nn.DataParallel(model, [0])
 
 # disable the  gradients of not optomized layers
 turn_off(model)
 
 model.load_state_dict(torch.load(options.w))
 
-checkpoint_dir = 'checkpoint/fo=%d/'% options.fold
+checkpoint_dir = 'checkpoint/fo=%d/' % options.fold
 check_dir(checkpoint_dir)
-
-
-
-
 
 
 # loading data
@@ -131,64 +136,54 @@ check_dir(checkpoint_dir)
 # valset
 # this only a quick val dataset where all images are 321*321.
 valset = Dataset_val(data_dir=data_dir, fold=options.fold, input_size=input_size, normalize_mean=IMG_MEAN,
-                 normalize_std=IMG_STD)
+                     normalize_std=IMG_STD)
 valloader = data.DataLoader(valset, batch_size=options.bs_val, shuffle=False, num_workers=4,
                             drop_last=False)
 
-save_pred_every =len(trainloader)
-
-
-
 
 optimizer = optim.SGD([{'params': get_10x_lr_params(model), 'lr': 10 * learning_rate}],
-                          lr=learning_rate, momentum=momentum, weight_decay=weight_decay)
+                      lr=learning_rate, momentum=momentum, weight_decay=weight_decay)
 
 
-
-
-loss_list = []#track training loss
-iou_list = []#track validaiton iou
+loss_list = []  # track training loss
+iou_list = []  # track validaiton iou
 highest_iou = 0
-
-
-
-
-
-
 
 
 model.cuda()
 tempory_loss = 0  # accumulated loss
 model = model.train()
-best_epoch=0
-for epoch in range(0,num_epoch):
-
-
+best_epoch = 0
+for epoch in range(0, num_epoch):
     begin_time = time.time()
-    tqdm_gen = tqdm.tqdm(trainloader)
 
     # ======================evaluate now==================
     with torch.no_grad():
-        print ('----Evaluation----')
+        print('----Evaluation----')
         model = model.eval()
 
-        valset.history_mask_list=[None] * 1000
+        valset.history_mask_list = [None] * 1000
         best_iou = 0
         for eva_iter in range(options.iter_time):
             all_inter, all_union, all_predict = [0] * 5, [0] * 5, [0] * 5
-            for i_iter, batch in enumerate(valloader):
+            for i_iter, batch in enumerate(tqdm.tqdm(valloader)):
+                if i_iter != 871:
+                    continue
 
                 query_rgb, query_mask, support_rgb, support_mask, history_mask, sample_class, index = batch
 
                 query_rgb = (query_rgb).cuda(0)
                 support_rgb = (support_rgb).cuda(0)
                 support_mask = (support_mask).cuda(0)
-                query_mask = (query_mask).cuda(0).long()  # change formation for crossentropy use
+                # change formation for crossentropy use
+                query_mask = (query_mask).cuda(0).long()
 
-                query_mask = query_mask[:, 0, :, :]  # remove the second dim,change formation for crossentropy use
+                # remove the second dim,change formation for crossentropy use
+                query_mask = query_mask[:, 0, :, :]
                 history_mask = (history_mask).cuda(0)
 
-                pred = model(query_rgb, support_rgb, support_mask,history_mask)
+                pred = model(query_rgb, support_rgb,
+                             support_mask, history_mask)
                 pred_softmax = F.softmax(pred, dim=1).data.cpu()
 
                 # update history mask
@@ -197,14 +192,31 @@ for epoch in range(0,num_epoch):
                     valset.history_mask_list[sub_index] = pred_softmax[j]
 
                     pred = nn.functional.interpolate(pred, size=query_mask.shape[-2:], mode='bilinear',
-                                                     align_corners=True)  #upsample  # upsample
+                                                     align_corners=True)  # upsample  # upsample
 
                 _, pred_label = torch.max(pred, 1)
-                inter_list, union_list, _, num_predict_list = get_iou_v1(query_mask, pred_label)
-                for j in range(query_mask.shape[0]):#batch size
-                    all_inter[sample_class[j] - (options.fold * 5 + 1)] += inter_list[j]
-                    all_union[sample_class[j] - (options.fold * 5 + 1)] += union_list[j]
 
+                plt.subplot(1, 2, 1)
+                plt.imshow(convert_image_np(support_rgb[0].cpu()))
+                plt.imshow(support_mask[0][0].cpu(), alpha=0.5)
+
+                plt.subplot(1, 2, 2)
+                plt.imshow(convert_image_np(query_rgb[0].cpu()))
+                plt.imshow(pred_label[0].cpu(), alpha=0.5)
+
+                plt.tight_layout()
+                plt.savefig(f'viz{options.fold}/{i_iter:03d}')
+                # plt.show()
+                plt.close()
+
+                _, pred_label = torch.max(pred, 1)
+                inter_list, union_list, _, num_predict_list = get_iou_v1(
+                    query_mask, pred_label)
+                for j in range(query_mask.shape[0]):  # batch size
+                    all_inter[sample_class[j] -
+                              (options.fold * 5 + 1)] += inter_list[j]
+                    all_union[sample_class[j] -
+                              (options.fold * 5 + 1)] += union_list[j]
 
             IOU = [0] * 5
 
@@ -212,19 +224,17 @@ for epoch in range(0,num_epoch):
                 IOU[j] = all_inter[j] / all_union[j]
 
             mean_iou = np.mean(IOU)
+            print(IOU)
             print('IOU:%.4f' % (mean_iou))
             if mean_iou > best_iou:
                 best_iou = mean_iou
             else:
                 break
 
-
-
-
         iou_list.append(best_iou)
         plot_iou(checkpoint_dir, iou_list)
         #np.savetxt(os.path.join(checkpoint_dir, 'iou_history.txt'), np.array(iou_list))
-        if best_iou>highest_iou:
+        if best_iou > highest_iou:
             highest_iou = best_iou
             model = model.eval()
             #torch.save(model.cpu().state_dict(), osp.join(checkpoint_dir, 'model', 'best' '.pth'))
@@ -232,20 +242,12 @@ for epoch in range(0,num_epoch):
             best_epoch = epoch
             print('A better model is saved')
 
-
-
         print('IOU for this epoch: %.4f' % (best_iou))
-
 
         model = model.train()
         model.cuda()
-
-
 
     epoch_time = time.time() - begin_time
     print('best epoch:%d ,iout:%.4f' % (best_epoch, highest_iou))
     print('This epoch taks:', epoch_time, 'second')
     print('still need hour:%.4f' % ((num_epoch - epoch) * epoch_time / 3600))
-
-
-
