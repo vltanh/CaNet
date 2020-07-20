b776b10fc16949f406690e2b788284da0a474b5f
vltanh <narubees@gmail.com>
2020-07-20 10:31:48 +0700

    clean codes

--- train.py
+++ train.py
@@ -16,68 +16,37 @@ from one_shot_network import Res_Deeplab
 import torch.nn as nn
 import numpy as np
 
-
-
+### Parse CMD arguments
 parser = argparse.ArgumentParser()
-
-
-parser.add_argument('-lr',
+parser.add_argument('-lr', 
                     type=float,
                     help='learning rate',
                     default=0.00025)
-
-parser.add_argument('-prob',
+parser.add_argument('-prob', 
                     type=float,
                     help='dropout rate of history mask',
                     default=0.7)
-
-
-parser.add_argument('-bs',
+parser.add_argument('-bs', 
                     type=int,
-                    help='batchsize',
+                    help='batch size in training',
                     default=4)
-
-parser.add_argument('-bs_val',
-                    type=int,
-                    help='batchsize for val',
-                    default=1)
-
-
-
 parser.add_argument('-fold',
                     type=int,
                     help='fold',
                     default=0)
-
-
-
 parser.add_argument('-gpu',
                     type=str,
                     help='gpu id to use',
                     default='0,1')
-
-
-
 parser.add_argument('-iter_time',
                     type=int,
+                    help='number of iterations for the IOM',
                     default=5)
-
 parser.add_argument('-data',
-                    type=str)
-
-
+                    type=str,
+                    help='path to the dataset folder')
 options = parser.parse_args()
 
-
-data_dir = options.data #'../davis_challenge_2020/data/PASCAL-5i/'
-
-
-
-
-#set gpus
-gpu_list = [int(x) for x in options.gpu.split(',')]
-os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
-os.environ['CUDA_VISIBLE_DEVICES'] = options.gpu
 
 def set_seed(seed):
     np.random.seed(seed)
@@ -89,8 +58,8 @@ def set_determinism():
     torch.backends.cudnn.benchmark = False
     torch.backends.cudnn.deterministic = True
 
-#set_determinism()
 
+### Constants/Variables
 IMG_MEAN = [0.485, 0.456, 0.406]
 IMG_STD = [0.229, 0.224, 0.225]
 num_class = 2
@@ -100,86 +69,78 @@ input_size = (321, 321)
 batch_size = options.bs
 weight_decay = 0.0005
 momentum = 0.9
-power = 0.9
 
+### GPU-related
+gpu_list = [int(x) for x in options.gpu.split(',')]
+os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
+os.environ['CUDA_VISIBLE_DEVICES'] = options.gpu
 cudnn.enabled = True
-
-
-set_seed(3698)
-# Create network.
-model = Res_Deeplab(num_classes=num_class)
-#load resnet-50 preatrained parameter
-model = load_resnet50_param(model, stop_layer='layer4')
-model=nn.DataParallel(model,[0])
-
-# disable the  gradients of not optomized layers
-turn_off(model)
-
-
 
 checkpoint_dir = 'checkpoint/fo=%d/'% options.fold
 check_dir(checkpoint_dir)
 
-
-
-
-
-
-
-
-# loading data
-
-# trainset
+### Network architecture
 set_seed(3698)
-dataset = Dataset_train(data_dir=data_dir, fold=options.fold, input_size=input_size, normalize_mean=IMG_MEAN,
-                  normalize_std=IMG_STD,prob=options.prob)
+model = Res_Deeplab(num_classes=num_class)
+model = load_resnet50_param(model, stop_layer='layer4')
+model = nn.DataParallel(model,[0])
+turn_off(model)
+
+### Dataset
+# Train
+set_seed(3698)
+dataset = Dataset_train(data_dir=options.data, fold=options.fold, 
+                        input_size=input_size, normalize_mean=IMG_MEAN, normalize_std=IMG_STD,
+                        prob=options.prob)
 trainloader = data.DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=4)
 
-# valset
-# this only a quick val dataset where all images are 321*321.
+# Validation
 set_seed(3698)
-valset = Dataset_val(data_dir=data_dir, fold=options.fold, input_size=input_size, normalize_mean=IMG_MEAN,
-                 normalize_std=IMG_STD)
-valloader = data.DataLoader(valset, batch_size=options.bs_val, shuffle=False, num_workers=4,
-                            drop_last=False)
+valset = Dataset_val(data_dir=options.data, fold=options.fold, 
+                     input_size=input_size, normalize_mean=IMG_MEAN, normalize_std=IMG_STD)
+valloader = data.DataLoader(valset, batch_size=1, shuffle=False, num_workers=4)
 
 save_pred_every =len(trainloader)
 
-optimizer = optim.SGD([{'params': get_10x_lr_params(model), 'lr': 10 * learning_rate}],
-                          lr=learning_rate, momentum=momentum, weight_decay=weight_decay)
+### Optimizer
+optimizer = optim.SGD([{'params': get_10x_lr_params(model), 
+                        'lr': 10 * learning_rate}],
+                      lr=learning_rate, momentum=momentum, weight_decay=weight_decay)
 
-loss_list = []#track training loss
-iou_list = []#track validaiton iou
+loss_list = []
+iou_list = []
 highest_iou = 0
 
 model.cuda()
-tempory_loss = 0  # accumulated loss
+tempory_loss = 0
 model = model.train()
 best_epoch=0
 for epoch in range(0,num_epoch):
     begin_time = time.time()
+
+    ### Train stage
+    model.train()
     tqdm_gen = tqdm.tqdm(trainloader)
     for i_iter, batch in enumerate(tqdm_gen):
-        query_rgb, query_mask,support_rgb, support_mask,history_mask,sample_class,index= batch
+        query_rgb, query_mask, support_rgb, support_mask, history_mask, sample_class, index = batch
 
         query_rgb = (query_rgb).cuda(0)
         support_rgb = (support_rgb).cuda(0)
         support_mask = (support_mask).cuda(0)
-        query_mask = (query_mask).cuda(0).long()  # change formation for crossentropy use
-        query_mask = query_mask[:, 0, :, :]  # remove the second dim,change formation for crossentropy use
-        history_mask=(history_mask).cuda(0)
+        query_mask = (query_mask).cuda(0).long()
+        query_mask = query_mask[:, 0, :, :] 
+        history_mask = (history_mask).cuda(0)
 
         optimizer.zero_grad()
 
-        pred=model(query_rgb, support_rgb, support_mask,history_mask)
-        pred_softmax=F.softmax(pred,dim=1).data.cpu()
+        pred = model(query_rgb, support_rgb, support_mask, history_mask)
+        pred_softmax = F.softmax(pred, dim=1).data.cpu()
 
         #update history mask
         for j in range (support_mask.shape[0]):
-            sub_index=index[j]
-            dataset.history_mask_list[sub_index]=pred_softmax[j]
-
-        pred = nn.functional.interpolate(pred,size=input_size, mode='bilinear',align_corners=True)#upsample
+            sub_index = index[j]
+            dataset.history_mask_list[sub_index] = pred_softmax[j]
+        pred = nn.functional.interpolate(pred,size=input_size, mode='bilinear',align_corners=True)
 
         loss = loss_calc_v1(pred, query_mask, 0)
         loss.backward()
@@ -191,32 +152,29 @@ for epoch in range(0,num_epoch):
         #save training loss
         tempory_loss += loss.item()
         if i_iter % save_pred_every == 0 and i_iter != 0:
-
             loss_list.append(tempory_loss / save_pred_every)
             plot_loss(checkpoint_dir, loss_list, save_pred_every)
             np.savetxt(os.path.join(checkpoint_dir, 'loss_history.txt'), np.array(loss_list))
             tempory_loss = 0
 
-    # ======================evaluate now==================
+    # Validation stage
     with torch.no_grad():
         print ('----Evaluation----')
-        model = model.eval()
+        model.eval()
 
-        valset.history_mask_list=[None] * 1000
+        valset.history_mask_list = [None] * 1000
         best_iou = 0
         for eva_iter in range(options.iter_time):
             all_inter, all_union, all_predict = [0] * 5, [0] * 5, [0] * 5
             for i_iter, batch in enumerate(valloader):
-
                 query_rgb, query_mask, support_rgb, support_mask, history_mask, sample_class, index = batch
 
-                query_rgb = (query_rgb).cuda(0)
-                support_rgb = (support_rgb).cuda(0)
-                support_mask = (support_mask).cuda(0)
-                query_mask = (query_mask).cuda(0).long()  # change formation for crossentropy use
-
-                query_mask = query_mask[:, 0, :, :]  # remove the second dim,change formation for crossentropy use
-                history_mask = (history_mask).cuda(0)
+                query_rgb = query_rgb.cuda(0)
+                support_rgb = support_rgb.cuda(0)
+                support_mask = support_mask.cuda(0)
+                query_mask = query_mask.cuda(0).long()
+                query_mask = query_mask[:, 0, :, :]
+                history_mask = history_mask.cuda(0)
 
                 pred = model(query_rgb, support_rgb, support_mask,history_mask)
                 pred_softmax = F.softmax(pred, dim=1).data.cpu()
@@ -225,21 +183,19 @@ for epoch in range(0,num_epoch):
                 for j in range(support_mask.shape[0]):
                     sub_index = index[j]
                     valset.history_mask_list[sub_index] = pred_softmax[j]
-
-                    pred = nn.functional.interpolate(pred, size=query_rgb.shape[-2:], mode='bilinear',
-                                                     align_corners=True)  #upsample  # upsample
-
+                    pred = nn.functional.interpolate(pred, 
+                                                     size=query_rgb.shape[-2:], 
+                                                     mode='bilinear',
+                                                     align_corners=True)
                 _, pred_label = torch.max(pred, 1)
                 inter_list, union_list, _, num_predict_list = get_iou_v1(query_mask, pred_label)
-                for j in range(query_mask.shape[0]):#batch size
+                for j in range(query_mask.shape[0]):
                     all_inter[sample_class[j] - (options.fold * 5 + 1)] += inter_list[j]
                     all_union[sample_class[j] - (options.fold * 5 + 1)] += union_list[j]
 
             IOU = [0] * 5
-
             for j in range(5):
                 IOU[j] = all_inter[j] / all_union[j]
-
             mean_iou = np.mean(IOU)
             print('IOU:%.4f' % (mean_iou))
             if mean_iou > best_iou:
@@ -258,8 +214,6 @@ for epoch in range(0,num_epoch):
             best_epoch = epoch
             print('A better model is saved')
         print('IOU for this epoch: %.4f' % (best_iou))
-        model = model.train()
-        model.cuda()
     epoch_time = time.time() - begin_time
     print('best epoch:%d ,iout:%.4f' % (best_epoch, highest_iou))
     print('This epoch taks:', epoch_time, 'second')
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import torchvision
 import os
 import torch
+import torch.nn as nn
 from pylab import plt
 
 
@@ -8,13 +9,9 @@ def load_resnet50_param(model, stop_layer='layer4'):
     resnet50 = torchvision.models.resnet50(pretrained=True)
     saved_state_dict = resnet50.state_dict()
     new_params = model.state_dict().copy()
-
-    for i in saved_state_dict:  # copy params from resnet50,except layers after stop_layer
-
+    for i in saved_state_dict:
         i_parts = i.split('.')
-
         if not i_parts[0] == stop_layer:
-
             new_params['.'.join(i_parts)] = saved_state_dict[i]
         else:
             break
@@ -29,8 +26,6 @@ def check_dir(checkpoint_dir):#create a dir if dir not exists
         os.makedirs(os.path.join(checkpoint_dir,'pred_img'))
 
 
-
-
 def optim_or_not(model, yes):
     for param in model.parameters():
         if yes:
@@ -41,18 +36,13 @@ def optim_or_not(model, yes):
 
 def turn_off(model):
     optim_or_not(model.module.conv1, False)
+    optim_or_not(model.module.bn1, False)
     optim_or_not(model.module.layer1, False)
     optim_or_not(model.module.layer2, False)
     optim_or_not(model.module.layer3, False)
 
 
-
-
 def get_10x_lr_params(model):
-    """
-    get layers for optimization
-    """
-
     b = []
     b.append(model.module.layer5_K.parameters())
     b.append(model.module.layer5_V.parameters())
@@ -73,61 +63,57 @@ def get_10x_lr_params(model):
             yield i
 
 
-
-
 def loss_calc_v1(pred, label, gpu):
-
     label = label.long()
     criterion = torch.nn.CrossEntropyLoss(ignore_index=255).cuda(gpu)
-
     return criterion(pred, label)
 
 
-
-def plot_loss(checkpoint_dir,loss_list,save_pred_every):
-    x=range(0,len(loss_list)*save_pred_every,save_pred_every)
-    y=loss_list
+def plot_loss(checkpoint_dir, loss_list, save_pred_every):
+    n = len(loss_list)
+    x = range(0, n * save_pred_every, save_pred_every)
+    y = loss_list
     plt.switch_backend('agg')
-    plt.plot(x,y,color='blue',marker='o',label='Train loss')
-    plt.xticks(range(0,len(loss_list)*save_pred_every+3,(len(loss_list)*save_pred_every+10)//10))
+    plt.plot(x, y, color='blue', marker='.', label='Train loss')
+    plt.xticks(
+        range(0, n * save_pred_every + 3,
+              (n * save_pred_every + 10) // 10)
+    )
     plt.legend()
     plt.grid()
-    plt.savefig(os.path.join(checkpoint_dir,'loss_fig.pdf'))
+    plt.savefig(os.path.join(checkpoint_dir, 'loss_fig.pdf'))
     plt.close()
 
 
-def plot_iou(checkpoint_dir,iou_list):
-    x=range(0,len(iou_list))
-    y=iou_list
+def plot_iou(checkpoint_dir, iou_list):
+    n = len(iou_list)
+    x = range(0, len(iou_list))
+    y = iou_list
     plt.switch_backend('agg')
-    plt.plot(x,y,color='red',marker='.',label='IOU')
-    plt.xticks(range(0,len(iou_list)+3,(len(iou_list)+10)//10))
+    plt.plot(x, y, color='red', marker='.', label='IOU')
+    plt.xticks(range(0, n + 3, (n + 10) // 10))
     plt.legend()
     plt.grid()
-    plt.savefig(os.path.join(checkpoint_dir,'iou_fig.pdf'))
+    plt.savefig(os.path.join(checkpoint_dir, 'iou_fig.pdf'))
     plt.close()
 
 
-
-def get_iou_v1(query_mask,pred_label,mode='foreground'):#pytorch 1.0 version
-    if mode=='background':
-        query_mask=1-query_mask
-        pred_label=1-pred_label
-    num_img=query_mask.shape[0]#batch size
-    num_predict_list,inter_list,union_list,iou_list=[],[],[],[]
-    for i in range(num_img):
-        num_predict=torch.sum((pred_label[i]>0).float()).item()
-        combination = (query_mask[i] + pred_label[i]).float()
-        inter = torch.sum((combination == 2).float()).item()
-        union = torch.sum((combination ==1).float()).item()+torch.sum((combination ==2).float()).item()
-        if union!=0:
-            inter_list.append(inter)
-            union_list.append(union)
-            iou_list.append(inter/union)
-            num_predict_list.append(num_predict)
+def get_iou_v1(query_mask, pred_label, mode='foreground'):
+    if mode == 'background':
+        query_mask = 1 - query_mask
+        pred_label = 1 - pred_label
+    B = query_mask.shape[0]
+    num_predict_list, inter_list, union_list, iou_list = [], [], [], []
+    for i in range(B):
+        num_predict = (pred_label[i] > 0).sum().item()
+        combination = query_mask[i] + pred_label[i]
+        inter = (combination == 2).sum().item()
+        union = (combination == 1).sum().item() + inter
+        inter_list.append(inter.float())
+        union_list.append(union.float())
+        num_predict_list.append(num_predict.float())
+        if union != 0:
+            iou_list.append(inter / union)
         else:
-            inter_list.append(inter)
-            union_list.append(union)
-            iou_list.append(0)
-            num_predict_list.append(num_predict)
-    return inter_list,union_list,iou_list,num_predict_list
+            iou_list.append(0.0)
+    return inter_list, union_list, iou_list, num_predict_list
