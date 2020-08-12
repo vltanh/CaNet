32ea9c4054de75c03dc746bdc15bba1df9259874
vltanh <narubees@gmail.com>
2020-08-12 13:25:44 +0700

    multiple runs with different seeds

--- val.py
+++ val.py
@@ -42,6 +42,9 @@ parser.add_argument('-s',
 parser.add_argument('-a',
                     action='store_true',
                     help='use attention or not')
+parser.add_argument('-p',
+                    type=int,
+                    help='number of exps')
 options = parser.parse_args()
 
 
@@ -70,98 +73,107 @@ model.load_state_dict(torch.load(options.w))
 model.cuda()
 
 set_seed(options.s)
+seeds = random.sample(range(10**5), options.p)
+print(seeds)
 
-valset = Dataset_val(data_dir=options.d, fold=options.fold,
-                     input_size=input_size,
-                     normalize_mean=IMG_MEAN, normalize_std=IMG_STD)
-valloader = data.DataLoader(valset, batch_size=1, shuffle=False, num_workers=4,
-                            drop_last=False)
+final_iou = []
+for s in seeds:
+    set_seed(s)
 
-iou_list = []
-highest_iou = 0
-begin_time = time.time()
-with torch.no_grad():
-    print('----Evaluation----')
-    model = model.eval()
+    valset = Dataset_val(data_dir=options.d, fold=options.fold,
+                         input_size=input_size,
+                         normalize_mean=IMG_MEAN, normalize_std=IMG_STD)
+    valloader = data.DataLoader(valset, batch_size=1, shuffle=False, num_workers=4,
+                                drop_last=False)
 
-    valset.history_mask_list = [None] * 1000
-    best_iou = 0
-    for eva_iter in range(options.iter_time):
-        save_root = f'viz{options.fold}_{options.a}'
-        save_dir = f'{save_root}/{eva_iter}'
-        os.makedirs(save_dir, exist_ok=True)
-        f = open(
-            f'{save_root}/score{options.fold}_{eva_iter}.csv', 'w')
-        f.write('support,query,class,score\n')
+    iou_list = []
+    highest_iou = 0
+    begin_time = time.time()
+    with torch.no_grad():
+        print('----Evaluation----')
+        model = model.eval()
 
-        all_inter, all_union, all_predict = [0] * 5, [0] * 5, [0] * 5
-        for i_iter, batch in enumerate(tqdm.tqdm(valloader)):
-            # if i_iter != 55:
-            #     continue
+        valset.history_mask_list = [None] * 1000
+        best_iou = 0
+        for eva_iter in range(options.iter_time):
+            save_root = f'viz{options.fold}_{options.a}'
+            save_dir = f'{save_root}/{eva_iter}'
+            os.makedirs(save_dir, exist_ok=True)
+            #f = open(
+            #    f'{save_root}/score{options.fold}_{eva_iter}.csv', 'w')
+            #f.write('support,query,class,score\n')
 
-            query_rgb, query_mask, support_rgb, support_mask, history_mask, sample_class, index, support_name, query_name = batch
+            all_inter, all_union, all_predict = [0] * 5, [0] * 5, [0] * 5
+            for i_iter, batch in enumerate(tqdm.tqdm(valloader)):
+                # if i_iter != 55:
+                #     continue
 
-            query_rgb = (query_rgb).cuda(0)
-            support_rgb = (support_rgb).cuda(0)
-            support_mask = (support_mask).cuda(0)
-            # change formation for crossentropy use
-            query_mask = (query_mask).cuda(0).long()
+                query_rgb, query_mask, support_rgb, support_mask, history_mask, sample_class, index, support_name, query_name = batch
 
-            # remove the second dim,change formation for crossentropy use
-            query_mask = query_mask[:, 0, :, :]
-            history_mask = (history_mask).cuda(0)
+                query_rgb = (query_rgb).cuda(0)
+                support_rgb = (support_rgb).cuda(0)
+                support_mask = (support_mask).cuda(0)
+                # change formation for crossentropy use
+                query_mask = (query_mask).cuda(0).long()
 
-            pred = model(query_rgb, support_rgb,
-                         support_mask, history_mask)
-            pred_softmax = F.softmax(pred, dim=1).data.cpu()
+                # remove the second dim,change formation for crossentropy use
+                query_mask = query_mask[:, 0, :, :]
+                history_mask = (history_mask).cuda(0)
 
-            # update history mask
-            for j in range(support_mask.shape[0]):
-                sub_index = index[j]
-                valset.history_mask_list[sub_index] = pred_softmax[j]
+                pred = model(query_rgb, support_rgb,
+                             support_mask, history_mask)
+                pred_softmax = F.softmax(pred, dim=1).data.cpu()
 
-                pred = nn.functional.interpolate(pred, size=query_mask.shape[-2:], mode='bilinear',
-                                                 align_corners=True)  # upsample  # upsample
+                # update history mask
+                for j in range(support_mask.shape[0]):
+                    sub_index = index[j]
+                    valset.history_mask_list[sub_index] = pred_softmax[j]
 
-            _, pred_label = torch.max(pred, 1)
+                    pred = nn.functional.interpolate(pred, size=query_mask.shape[-2:], mode='bilinear',
+                                                     align_corners=True)  # upsample  # upsample
 
-            plt.subplot(1, 2, 1)
-            plt.imshow(convert_image_np(support_rgb[0].cpu()))
-            plt.imshow(support_mask[0][0].cpu(), alpha=0.5)
+                _, pred_label = torch.max(pred, 1)
 
-            plt.subplot(1, 2, 2)
-            plt.imshow(convert_image_np(query_rgb[0].cpu()))
-            plt.imshow(pred_label[0].cpu(), alpha=0.5)
+                #plt.subplot(1, 2, 1)
+                #plt.imshow(convert_image_np(support_rgb[0].cpu()))
+                #plt.imshow(support_mask[0][0].cpu(), alpha=0.5)
 
-            plt.tight_layout()
-            plt.savefig(f'{save_dir}/{i_iter:03d}')
-            # plt.show()
-            plt.close()
+                #plt.subplot(1, 2, 2)
+                #plt.imshow(convert_image_np(query_rgb[0].cpu()))
+                #plt.imshow(pred_label[0].cpu(), alpha=0.5)
 
-            _, pred_label = torch.max(pred, 1)
-            inter_list, union_list, _, num_predict_list = get_iou_v1(
-                query_mask, pred_label)
-            f.write(
-                f'{support_name[0]},{query_name[0]},{sample_class[0]},{float(inter_list[0])/union_list[0]}\n')
-            for j in range(query_mask.shape[0]):  # batch size
-                all_inter[sample_class[j] -
-                          (options.fold * 5 + 1)] += inter_list[j]
-                all_union[sample_class[j] -
-                          (options.fold * 5 + 1)] += union_list[j]
+                #plt.tight_layout()
+                #plt.savefig(f'{save_dir}/{i_iter:03d}')
+                ## plt.show()
+                #plt.close()
 
-        IOU = [0] * 5
+                _, pred_label = torch.max(pred, 1)
+                inter_list, union_list, _, num_predict_list = get_iou_v1(
+                    query_mask, pred_label)
+                #f.write(
+                #    f'{support_name[0]},{query_name[0]},{sample_class[0]},{float(inter_list[0])/union_list[0]}\n')
+                for j in range(query_mask.shape[0]):  # batch size
+                    all_inter[sample_class[j] -
+                              (options.fold * 5 + 1)] += inter_list[j]
+                    all_union[sample_class[j] -
+                              (options.fold * 5 + 1)] += union_list[j]
 
-        for j in range(5):
-            IOU[j] = all_inter[j] / all_union[j]
+            IOU = [0] * 5
 
-        mean_iou = np.mean(IOU)
-        print(IOU)
-        print('IOU:%.4f' % (mean_iou))
-        if mean_iou > best_iou:
+            for j in range(5):
+                IOU[j] = all_inter[j] / all_union[j]
+
+            mean_iou = np.mean(IOU)
+            print(IOU)
+            print('IOU:%.4f' % (mean_iou))
+            #if mean_iou > best_iou:
+            #    best_iou = mean_iou
+
+            #f.close()
             best_iou = mean_iou
+        print('IOU for this epoch: %.4f' % (best_iou))
+        final_iou.append(best_iou)
 
-        f.close()
-    print('IOU for this epoch: %.4f' % (best_iou))
-
-epoch_time = time.time() - begin_time
-print('This epoch takes:', epoch_time, 'second')
+    epoch_time = time.time() - begin_time
+    print('This epoch takes:', epoch_time, 'second')
+    print(np.mean(final_iou), np.std(final_iou))
